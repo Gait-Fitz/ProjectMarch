41ca4b6ff4fae8c97dddeaa73f04c478f66838a0
Maarten ten Voorde <m.tenvoorde1998@gmail.com>
2021-04-15 10:32:08 +0200

    Removed dubble shift and mpc now outputs a vector

--- ros1/src/control/march_acado_mpc/src/joint_trajectory_mpc.cpp
+++ ros1/src/control/march_acado_mpc/src/joint_trajectory_mpc.cpp
@@ -258,7 +258,8 @@ void ModelPredictiveControllerInterface::updateCommand(
     // Calculate mpc and apply command
     command = model_predictive_controller_->calculateControlInput();
 
-    ROS_INFO_STREAM("COMMAND CALCULATED: " << acadoVariables.u[0] << ", COMMAND SEND: " << command);
+    ROS_INFO_STREAM("COMMAND CALCULATED: " << acadoVariables.u[0]
+                                           << ", COMMAND SEND: " << command[0]);
 
     for (unsigned int i = 0; i < num_joints_; ++i) {
         // Apply command
