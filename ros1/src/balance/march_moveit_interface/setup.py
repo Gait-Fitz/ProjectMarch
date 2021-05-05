8f168fd92ec2f7fb59d28291949796a28fddcf70
Thijs Raymakers <thijs@raymakers.nl>
2021-05-05 17:10:47 +0200

    Add GPLv3 copyright notice at the top of each relevant file in ROS1 and ROS2

--- ros1/src/balance/march_moveit_interface/setup.py
+++ ros1/src/balance/march_moveit_interface/setup.py
@@ -1,3 +1,22 @@
+# Copyright (C) 2021 Katja Schmahl
+#
+# This program is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# Version 3 as published by the Free Software Foundation WITH
+# additional terms published by Project MARCH per section 7 of
+# the GNU General Public License Version 3.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License INCLUDING the additional terms for
+# more details.
+#
+# You should have received a copy of the GNU General Public License
+# AND the additional terms along with this program. If not,
+# see <https://projectmarch.nl/s/LICENSE> and
+# <https://projectmarch.nl/s/LICENSE-ADDITIONAL-TERMS>.
+
 # ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
 from catkin_pkg.python_setup import generate_distutils_setup
 from setuptools import setup
