d1caace917323d85741bdd1048a762420ac18b26
Maarten ten Voorde <m.tenvoorde1998@gmail.com>
2021-08-13 12:35:07 +0200

    Working awgn publisher node

---
+++ ros1/src/control/march_system_identification/src/march_system_identification/frequency_response_analysis.py
@@ -0,0 +1,27 @@
+# FRF_AGWN : Frequency Response function with additive gaussian white
+# noise for system identification
+
+import numpy as np
+from numpy.random import sample
+
+def additive_gaussian_white_noise():
+
+    mean = 0.0
+    std = 1.0
+    num_samples = 1
+    samples = np.random.normal(mean, std, size=num_samples)
+
+    return samples
+
+
+def main():
+
+    feedforward = additive_gaussian_white_noise()
+    print(feedforward)
+
+    return 0
+
+
+if __name__ == '__main__':
+    print("executing main() in frequency_response_analysis")
+    main()
--- ros1/src/control/march_system_identification/src/march_system_identification/system_identification_node.py
+++
@@ -1,6 +0,0 @@
-
-from march_system_identification import frf_agwn
-
-def main():
-    frf_agwn.main()
-    print("system_identification_node")
