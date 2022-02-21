31687f1e7892e419b779067e05b53ab65a819ff7
Marten Haitjema <martenh99@gmail.com>
2022-02-21 14:07:12 +0100

    Refactor grid search

--- ros2/src/shared/march_utility/march_utility/gait/gait_parameter_limit_setter.py
+++ ros2/src/shared/march_utility/march_utility/gait/gait_parameter_limit_setter.py
@@ -65,33 +65,28 @@ class SetGaitParameterLimits:
         Sets the min and max limits and writes this to a yaml file."""
         self.reset()
         self.middle_point_height = []
-        middle_point_height_values = self.create_linspace(0.1, 0.5, 9)
-        for value in middle_point_height_values:
-            self.set_middle_point_height(value)
+        middle_point_height_values = self.create_linspace(0.1, 0.5, 17)
+        self.set_middle_point_height(middle_point_height_values)
 
         self.reset()
         self.middle_point_fraction = []
-        middle_point_fraction_values = self.create_linspace(0.4, 0.7, 6)
-        for value in middle_point_fraction_values:
-            self.set_middle_point_fraction(value)
+        middle_point_fraction_values = self.create_linspace(0.4, 0.7, 11)
+        self.set_middle_point_fraction(middle_point_fraction_values)
 
         self.reset()
         self.push_off_position = []
-        push_off_position_values = self.create_linspace(0.0, 0.5, 11)
-        for value in push_off_position_values:
-            self.set_push_off_position(value)
+        push_off_position_values = self.create_linspace(0.0, 0.5, 21)
+        self.set_push_off_position(push_off_position_values)
 
         self.reset()
         self.push_off_fraction = []
-        push_off_fraction_values = self.create_linspace(0.05, 0.3, 6)
-        for value in push_off_fraction_values:
-            self.set_push_off_fraction(value)
+        push_off_fraction_values = self.create_linspace(0.05, 0.3, 11)
+        self.set_push_off_fraction(push_off_fraction_values)
 
         self.reset()
         self.duration = []
         times = self.create_linspace(0.5, 5.0, 91)
-        for time in times:
-            self.set_duration(time)
+        self.set_duration(times)
 
         self._limits = {
             "middle_point_height": [
@@ -113,40 +108,88 @@ class SetGaitParameterLimits:
             "dynamic_subgait_duration": [min(self.duration), max(self.duration)],
         }
 
-    def set_middle_point_height(self, value: float) -> None:
+    def set_middle_point_height(self, values: List[float]) -> None:
         """Set the parameter to given value and try to get trajectory.
         Append to list of possible values if successful."""
-        self.dynamic_subgait.middle_point_height = value
-        if self.try_to_get_trajectories():
-            self.middle_point_height.append(value)
+        for value in values:
+            self.dynamic_subgait.middle_point_height = value
+            if self.try_to_get_stair_trajectory():
+                self.gait.logger.info(f"Appending value {value}")
+                self.middle_point_height.append(value)
 
-    def set_middle_point_fraction(self, value: float) -> None:
-        """Set the parameter to given value and try to get trajectory.
-        Append to list of possible values if successful."""
-        self.dynamic_subgait.middle_point_fraction = value
-        if self.try_to_get_trajectories():
-            self.middle_point_fraction.append(value)
+        for value in self.middle_point_height:
+            if not self.try_to_get_home_stand_trajectory():
+                self.gait.logger.info(f"Removing value {value}")
+                self.middle_point_height.remove(value)
 
-    def set_push_off_position(self, value: float) -> None:
-        """Set the parameter to given value and try to get trajectory.
-        Append to list of possible values if successful."""
-        self.dynamic_subgait.push_off_position = -value
-        if self.try_to_get_trajectories():
-            self.push_off_position.append(value)
+        for value in self.middle_point_height:
+            if not self.try_to_get_normal_trajectory():
+                self.gait.logger.info(f"Removing value {value}")
+                self.middle_point_height.remove(value)
 
-    def set_push_off_fraction(self, value: float) -> None:
+    def set_middle_point_fraction(self, values: List[float]) -> None:
         """Set the parameter to given value and try to get trajectory.
         Append to list of possible values if successful."""
-        self.dynamic_subgait.push_off_fraction = value
-        if self.try_to_get_trajectories():
-            self.push_off_fraction.append(value)
+        for value in values:
+            self.dynamic_subgait.middle_point_fraction = value
+            if self.try_to_get_stair_trajectory():
+                self.middle_point_fraction.append(value)
 
-    def set_duration(self, value: float) -> None:
+        for value in self.middle_point_fraction:
+            if not self.try_to_get_home_stand_trajectory():
+                self.middle_point_fraction.remove(value)
+
+        for value in self.middle_point_fraction:
+            if not self.try_to_get_normal_trajectory():
+                self.middle_point_fraction.remove(value)
+
+    def set_push_off_position(self, values: List[float]) -> None:
         """Set the parameter to given value and try to get trajectory.
         Append to list of possible values if successful."""
-        self.dynamic_subgait.duration = value
-        if self.try_to_get_trajectories():
-            self.duration.append(value)
+        for value in values:
+            self.dynamic_subgait.push_off_position = value
+            if self.try_to_get_stair_trajectory():
+                self.push_off_position.append(value)
+
+        for value in self.push_off_position:
+            if not self.try_to_get_home_stand_trajectory():
+                self.push_off_position.remove(value)
+
+        for value in self.push_off_position:
+            if not self.try_to_get_normal_trajectory():
+                self.push_off_position.remove(value)
+
+    def set_push_off_fraction(self, values: List[float]) -> None:
+        """Set the parameter to given value and try to get trajectory.
+        Append to list of possible values if successful."""
+        for value in values:
+            self.dynamic_subgait.push_off_fraction = value
+            if self.try_to_get_stair_trajectory():
+                self.push_off_fraction.append(value)
+
+        for value in self.push_off_fraction:
+            if not self.try_to_get_home_stand_trajectory():
+                self.push_off_fraction.remove(value)
+
+        for value in self.push_off_fraction:
+            if not self.try_to_get_normal_trajectory():
+                self.push_off_fraction.remove(value)
+
+    def set_duration(self, values: List[float]) -> None:
+        """Set the parameter to given value and try to get trajectory.
+        Append to list of possible values if successful."""
+        for value in values:
+            self.dynamic_subgait.dynamic_subgait_duration = value
+            if self.try_to_get_stair_trajectory():
+                self.duration.append(value)
+
+        for value in self.duration:
+            if not self.try_to_get_home_stand_trajectory():
+                self.duration.remove(value)
+
+        for value in self.duration:
+            if not self.try_to_get_normal_trajectory():
+                self.duration.remove(value)
 
     def try_to_get_trajectory(self) -> bool:
         """Tries to create a trajectory with the set parameters. If
@@ -187,18 +230,26 @@ class SetGaitParameterLimits:
             for value in np.linspace(lower, upper, points).tolist()
         ]
 
-    def try_to_get_trajectories(self):
-        self.set_start_to_home_stand
+    def try_to_get_stair_trajectory(self) -> bool:
+        self.dynamic_subgait.location.x = 0.5
+        self.dynamic_subgait.location.y = 0.15
         if self.try_to_get_trajectory():
-            self.set_start_to_end()
-            if self.try_to_get_trajectory():
-                self.set_start_to_end()
-                self.dynamic_subgait.stop = True
-                if self.try_to_get_trajectory():
-                    self.dynamic_subgait.stop = False
-                    return True
-        self.dynamic_subgait.stop = False
-        return False
+            self.dynamic_subgait.starting_position = (
+                self.dynamic_subgait.get_final_position()
+            )
+            return self.try_to_get_trajectory()
+        else:
+            return False
+
+    def try_to_get_home_stand_trajectory(self) -> bool:
+        self.dynamic_subgait.location.x = 0.4
+        self.dynamic_subgait.location.y = 0.0
+        self.set_start_to_home_stand()
+        return self.try_to_get_trajectory()
+
+    def try_to_get_normal_trajectory(self) -> bool:
+        self.set_start_to_end()
+        return self.try_to_get_trajectory()
 
     def set_start_to_home_stand(self) -> None:
         self.dynamic_subgait.starting_position_position = (
