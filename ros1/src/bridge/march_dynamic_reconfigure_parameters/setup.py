086b65242913bf6db5ed06fea89222652a236f97
George <georgevegelien@gmail.com>
2022-03-08 17:50:13 +0100

    Fix bridging communication of dynamic_pid_reconfigure

--- ros1/src/bridge/march_dynamic_reconfigure_parameters/setup.py
+++ ros1/src/bridge/march_dynamic_reconfigure_parameters/setup.py
@@ -3,9 +3,9 @@ from catkin_pkg.python_setup import generate_distutils_setup
 from setuptools import setup
 
 setup_args = generate_distutils_setup(
-    packages=["dynamic_reconfigure_parameters"],
+    packages=["march_dynamic_reconfigure_parameters"],
     package_dir={"": "src"},
-    scripts=["src/dynamic_reconfigure_parameters/march_dynamic_reconfigure_node"],
+    scripts=["scripts/march_dynamic_reconfigure_parameters_node"],
 )
 
 setup(**setup_args)
