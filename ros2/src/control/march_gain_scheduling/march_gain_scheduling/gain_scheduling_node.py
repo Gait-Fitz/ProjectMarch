edb8ac51fcbf04b2aa0b77ed0253f49eda62e1a5
George <georgevegelien@gmail.com>
2022-03-10 12:29:35 +0100

    Make joint names dynamic

--- ros2/src/control/march_gain_scheduling/march_gain_scheduling/gain_scheduling_node.py
+++ ros2/src/control/march_gain_scheduling/march_gain_scheduling/gain_scheduling_node.py
@@ -2,6 +2,7 @@ import sys
 
 import rclpy
 
+from march_utility.utilities.utility_functions import get_joint_names_from_urdf
 from .dynamic_pid_reconfigurer import DynamicPIDReconfigurer
 
 
@@ -9,25 +10,7 @@ def main():
     rclpy.init(args=sys.argv)
     node = rclpy.create_node("march_gain_scheduling_node", automatically_declare_parameters_from_overrides=True)
     node.get_logger().info("Created node")
-
-    # rate = node.create_rate(0.5, node.get_clock())
-    # try:
-    #     while rclpy.ok() and not node.has_parameter("/march/joint_names"):
-    #         rate.sleep()
-    #         node.get_logger().debug("Waiting on /march/joint_names to be available")
-    # except KeyboardInterrupt:
-    #     pass
-    #
-    # # I think this is redundant due to the way spin works right?
-    # if not rclpy.ok():
-    #     return
-
-    # joint_list = node.declare_parameter("/march/joint_names").value
-
-
-    joint_list = ['left_ankle', 'left_hip_aa', 'left_hip_fe', 'left_knee', 'right_ankle',
-                  'right_hip_aa', 'right_hip_fe', 'right_knee']
-    DynamicPIDReconfigurer(joint_list, node)
+    DynamicPIDReconfigurer(get_joint_names_from_urdf(), node)
 
     rclpy.spin(node)
 
