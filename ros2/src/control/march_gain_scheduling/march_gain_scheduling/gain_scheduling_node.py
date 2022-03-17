5978f4d6657a523e3868e783a08b23d8070b1b9b
George <georgevegelien@gmail.com>
2022-03-17 10:52:47 +0100

    Add a clean close down for 'ctr + c'

--- ros2/src/control/march_gain_scheduling/march_gain_scheduling/gain_scheduling_node.py
+++ ros2/src/control/march_gain_scheduling/march_gain_scheduling/gain_scheduling_node.py
@@ -11,8 +11,10 @@ def main():
     node = rclpy.create_node("march_gain_scheduling_node", automatically_declare_parameters_from_overrides=True)
     node.get_logger().info("Created node")
     DynamicPIDReconfigurer(get_joint_names_from_urdf(), node)
-
-    rclpy.spin(node)
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        rclpy.shutdown()
 
 
 if __name__ == "__main__":
