3195a7b0b18ebfa360697f3d7c5f29be0fb95a42
Willemijn <willemijn.drenth@gmail.com>
2022-03-31 15:53:11 +0200

    Fix wording mistake

--- ros2/src/control/march_gain_scheduling/march_gain_scheduling/gain_scheduling_node.py
+++ ros2/src/control/march_gain_scheduling/march_gain_scheduling/gain_scheduling_node.py
@@ -8,7 +8,7 @@ from .dynamic_pid_reconfigurer import DynamicPIDReconfigurer
 
 
 def main():
-    """Starts the "march_gain_scheduling_node" and node and passes it to DynamicPIDReconfigurer."""
+    """Starts the "march_gain_scheduling_node" node and passes it to DynamicPIDReconfigurer."""
     rclpy.init(args=sys.argv)
     node = rclpy.create_node("march_gain_scheduling_node", automatically_declare_parameters_from_overrides=True)
     node.get_logger().info("Created node")
