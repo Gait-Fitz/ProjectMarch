60d37e74561d5c47245d81a940557635e51c8c18
George <georgevegelien@gmail.com>
2022-05-19 12:06:07 +0200

    Refactor file reading to use native python code

--- ros2/src/hardware/march_eeg/march_eeg/headset.py
+++ ros2/src/hardware/march_eeg/march_eeg/headset.py
@@ -1,4 +1,6 @@
-import subprocess
+"""Author: Eline Troquay, MVII; George Vegelien, MVII."""
+import os
+import subprocess  # noqa: S404. Subprocess can be unsafe but is necessary in this case.
 from time import sleep
 
 import mne
@@ -8,14 +10,15 @@ from march_eeg.eeg_utility import convert_to_list_of_floats
 
 
 class Headset:
-    amountOfChannels: int
+    """Abstract class for the headset data"""
+    number_of_channels: int
     sfreq: int
 
     def get_data(self, number_of_samples):
         raise NotImplementedError("This should be an abstract method")
 
     def preprocessing(self, data):
-        data = data[:, 0:self.amountOfChannels]
+        data = data[:, 0:self.number_of_channels]
         data = data.transpose()
 
         my_iir_params = mne.filter.construct_iir_filter(dict(order=2, ftype='butter'), [8, 30.0], None, self.sfreq,
@@ -45,7 +48,7 @@ class Nautilus(Headset):
     NAME = 'NAUTILUS'
 
     def __init__(self):
-        self.amountOfChannels = 16
+        self.number_of_channels = 16
         self.sfreq = 500
 
     def get_data(self, number_of_samples):
@@ -57,22 +60,22 @@ class Unicorn(Headset):
     BIN_SINGLE_LINE_BYTE_SIZE = 68
 
     def __init__(self, data_file_loc: str):
-        self.amountOfChannels = 8
+        self.number_of_channels = 8
         self.sfreq = 250
         self.data_file_loc = data_file_loc  # /home/george/repos/eeg/realtime/Unicorn Linux C API/x64/data.bin'
 
     def get_data(self, number_of_samples: int):
         sample_list = []
         line_byte_size = self.BIN_SINGLE_LINE_BYTE_SIZE * number_of_samples
-        bin_proces = subprocess.Popen(['tail', '-c', str(line_byte_size), '-f', self.data_file_loc]
-                                      , stdout=subprocess.PIPE, close_fds=True)
-        convert_proces = subprocess.Popen(['unicorn_converter', '-'], stdin=bin_proces.stdout,
-                                          stdout=subprocess.PIPE, close_fds=True, universal_newlines=True)
+        byte_offset = line_byte_size + (os.stat(self.data_file_loc).st_size % self.BIN_SINGLE_LINE_BYTE_SIZE)
+        with open(self.data_file_loc, 'rb') as file:
+            file.seek(-byte_offset, os.SEEK_END)
+            convert_proces = subprocess.Popen(['unicorn_converter', '-'], stdin=file,
+                                              stdout=subprocess.PIPE, close_fds=True, universal_newlines=True)
         for i, stdout_line in enumerate(iter(convert_proces.stdout.readline, "")):
             if len(sample_list) < number_of_samples:
                 sample_list.append(convert_to_list_of_floats(stdout_line))
             else:
-                bin_proces.terminate()
                 convert_proces.terminate()
                 break
         return np.array(sample_list)
